ef5270fc4c49a08ada7076e176c314406f010e9e
wastedsummer <stefan.wezel@student.uni-tuebingen.de>
2019-05-19 15:00:58 +0200

    added aufgabe 2

--- stocha_blatt_4.r
+++ stocha_blatt_4.r
@@ -1,12 +1,15 @@
-eq = function(x){x^(-x)}
 
+
+library("ggplot2")
+
+eq = function(x){x^(-x)}
 area = integrate(eq, 0, 4)
-print(area[1])
+# print(area[1])
 c = 1/as.numeric(area3[1])
 print(c)
 
-
-curve(eq, from=-1, to=5, xlab="x", ylab="y", 
+density = function(x){c * (x^(-x))}
+curve(density, from=-1, to=5, xlab="x", ylab="y", 
       xlim = c(-1, 6),
       ylim = c(0, 3),
       col = "coral",
@@ -15,12 +18,58 @@ curve(eq, from=-1, to=5, xlab="x", ylab="y",
       col.axis = "darkslategray",
       bty = "n")
 
-F = function(t){integrate(eq, limits = NULL, t)}
-curve(F, from=-10, to=10, xlab="x", ylab="y", 
-      xlim = c(-20, 20),
-      col = "coral",
-      col.lab="darkslategray",
-      col.main = "darkslategray",
-      col.axis = "darkslategray",
-      bty = "n")
+
+
+
+
+#f = function(t){integrate(density, lower = -Inf, upper = Inf, t)}
+F <- function(t){
+   if(t < 0){
+      return(0)
+   }
+   else if(t >=0 && t < 4){
+      return(as.numeric(integrate(density, lower = 0, upper = t)[1]))
+   }
+   else {
+      return(1)
+   }
+}
+
+
+ggplot(data.frame(x=c(-1, 5)), aes(x=x)) + 
+   stat_function(fun=Vectorize(F), geom="line", color = "coral") + 
+   xlab("x") + 
+   ylab("y")
+
+
+
+
+
+# Finv <- function(s){
+#    if(s<0){
+#       return(1)
+#    }
+#    else if(s >=0 && s < 4){
+#       return(uniroot(F, c(0,1)))
+#    }
+#    else{
+#       return(0)
+#    }
+# }
+
+Finv <- function(s){
+   return(uniroot(F, lower = -200, upper = 200))
+}
+
+
+print(Finv(1)$root)
+# ggplot(data.frame(x=c(0, 1)), aes(x=x)) +
+#    stat_function(fun=Vectorize(Finv), geom="line", color = "cornflowerblue") +
+#    xlab("x") +
+#    ylab("y")
+
+
+
+
+
 
