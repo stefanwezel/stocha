495cf7913d28b002b887d8fba6c742f024af4f23
wastedsummer <stefan.wezel@student.uni-tuebingen.de>
2019-06-17 13:48:42 +0200

    blatt7

---
+++ stocha_blatt_7.r
@@ -0,0 +1,16 @@
+  #x <- runif(1000000, min = 0, max = 1)
+ # y <- runif(1000000, min = 0, max = 1)
+  print("hey")
+  list = c()
+  #for (i in 1:1000000) {
+    #n <- (runif(1,0,1) + runif(1,0,1))
+   # list <- c(list, n)
+    #print(list)
+   # }
+  #print(head(y, n = 5))
+  #print(list)
+  #hist(y, freq = FALSE)
+  #hist(y)
+  #hist(list, freq = FALSE, b = 20)
+  print("done")
+  
